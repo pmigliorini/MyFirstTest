265fa0af9adf085627651305cac5aa45e29bc873
pmigliorini <36510839+pmigliorini@users.noreply.github.com>
2018-02-15 18:07:41 +0100

    add new version

--- App2/App2/App.xaml.cs
+++ App2/App2/App.xaml.cs
@@ -2,6 +2,9 @@
 
 using App2.Views;
 using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace App2
 {
@@ -18,8 +21,11 @@ namespace App2
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+            // Handle when your app starts
+
+            AppCenter.Start("ios=30ee31bd-a4c4-4efd-b34d-294e516f7eea;" + "android=4078c2da-6215-4e03-9470-387c7cc411c9;", typeof(Analytics), typeof(Crashes));
+
+        }
 
 		protected override void OnSleep ()
 		{
